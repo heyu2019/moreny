94332e7d4e66d6ec6c959eed4d065b8610f481ea
heyu2019 <615791941@qq.com>
2020-04-26 15:32:52 +0800

    创建icon组件，引入svg完全返回的目录

---
+++ src/components/Icons.vue
@@ -0,0 +1,24 @@
+<template>
+  <svg class="icon">
+    <use :xlink:href="'#'+name"/>
+  </svg>
+</template>
+
+<script lang="ts">
+  const importAll =(requireContext: __WebpackModuleApi.RequireContext) => requireContext.keys().forEach(requireContext);
+  try{importAll(require.context('../assets/icons',true,/\.svg$/))}catch (err) {console.log(err);} //实现完全返回import目录
+  export default {
+    props:['name'],
+    name: 'icons'
+  };
+</script>
+
+<style lang="scss" scoped>
+  .icon{
+    width: 1em;
+    height: 1em;
+    vertical-align: -0.15em;
+    overflow: hidden;
+    fill: currentColor;
+  }
+</style>
--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,17 +1,24 @@
 <template>
   <div class="nav">
-    <router-link to="/money">记账</router-link>
+    <router-link to="/money">
+      <Icon name="money"/>
+      记账
+    </router-link>
     |
-    <router-link to="/labels">标签</router-link>
+    <router-link to="/labels">
+      <Icon name="labels"/>
+      标签
+    </router-link>
     |
-    <router-link to="/statistic">统计 </router-link>
+    <router-link to="/statistic">
+      <Icon name="statistic"/>
+      统计
+    </router-link>
     |
   </div>
 </template>
 
 <script lang="ts">
-  import x from '@/assets/icons/labels.svg';
-  console.log(x)
   export default {
     name: 'Nav'
   };
