537ba84bfaba3324d3dc88bd50250c32dcaa0905
heyu2019 <615791941@qq.com>
2020-04-26 23:20:54 +0800

    写好money页面的html

--- src/App.vue
+++ src/App.vue
@@ -5,33 +5,14 @@
 </template>
 
 <style lang="scss">
+  @import "~@/assets/style/helper.scss";
+  @import "~@/assets/style/reset.scss";
   body {
-    margin: 0;padding: 0;
-    box-sizing: border-box;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    color: #333333;
     line-height: 1.5;
+    font-family: $fonts-hei;
+    font-size: 16px;
   }
-  a {
-    text-decoration: none;
-    color: inherit;
-  }
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
 </style>
