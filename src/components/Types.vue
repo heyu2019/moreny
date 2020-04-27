dcfef0ddac7c922535800783df998df1f82422eb
heyu2019 <615791941@qq.com>
2020-04-27 14:33:25 +0800

    模块化，把money页面拆分成对应的组件

---
+++ src/components/Types.vue
@@ -0,0 +1,43 @@
+<template>
+  <ul class="types">
+    <li class="selected">支出</li>
+    <li>收入</li>
+  </ul>
+</template>
+
+<script lang="ts">
+  export default {
+    name: 'types'
+  };
+</script>
+
+<style lang="scss" scoped>
+  .types {
+    background: #c4c4c4;
+    display: flex;
+    text-align: center;
+    font-size: 24px;
+
+    > li {
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      width: 50%;
+      height: 64px;
+
+      &.selected {
+        position: relative;
+
+        &::after {
+          content: '';
+          position: absolute;
+          bottom: 0;
+          left: 0;
+          width: 100%;
+          height: 4px;
+          background: #333333;
+        }
+      }
+    }
+  }
+</style>
