03b1e94cf8efbaa1c730e462923788e73026bd19
heyu2019 <615791941@qq.com>
2020-05-06 21:11:58 +0800

    把id存储到localStorage 删除formItem中多余的代码

--- src/components/Money/FormItem.vue
+++ src/components/Money/FormItem.vue
@@ -5,21 +5,20 @@
       <input type="text"
              :value="value"
              @input="onValueChanged($event.target.value)"
-             :placeholder="this.placeholder"/>
+             :placeholder="placeholder"/>
     </label>
   </div>
 </template>
 
 <script lang="ts">
   import Vue from 'vue';
-  import {Component, Prop, Watch} from 'vue-property-decorator';
+  import {Component, Prop} from 'vue-property-decorator';
 
   @Component
   export default class FormItem extends Vue{
     @Prop({default:''}) readonly value!: string;
     @Prop({required:true}) fileName!: string;
     @Prop() placeholder?: string;
-    @Watch('value')
     onValueChanged(Value: string){
       this.$emit('update:value',Value);
     }
--- src/lib/idCreater.ts
+++ src/lib/idCreater.ts
@@ -1,8 +1,10 @@
-let id: number = parseInt(window.localStorage.getItem('_idmax') || '0') || 0;
+let id: number = parseInt(window.localStorage.getItem('_idMax') || '0') || 0;
 //从localStorage里获取字符串如果获取不到就获取0 如果获取到了就尝试将字符串转换为number 如果不能转换就为0
 
 function createID() {
   id++;
+  //把id存储到localStorage
+  window.localStorage.setItem('_idMax',id.toString());
   return id;
 }
 
