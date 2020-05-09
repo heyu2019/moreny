fc4dbddf40f599a178aa337a49c3950dde86ab3b
heyu2019 <615791941@qq.com>
2020-05-09 22:35:34 +0800

    修复刷新变成404的bug

--- src/store/index.ts
+++ src/store/index.ts
@@ -2,11 +2,12 @@ import Vue from 'vue';
 import Vuex, {Store} from 'vuex';
 import clone from '@/lib/clone';
 import createID from '@/lib/idCreater';
+import router from '@/router';
 
 Vue.use(Vuex);
 type RooTState = {
   recordList: RecordItem[];
-  tagList:  Tag[];
+  tagList: Tag[];
   currentTag?: Tag;
 }
 const store = new Vuex.Store({
@@ -14,7 +15,7 @@ const store = new Vuex.Store({
     recordList: [] as RecordItem[],
     tagList: [] as Tag[],
     currentTag: undefined
-  }as RooTState,
+  } as RooTState,
   mutations: {
     fetchRecordList: function (state) {
       state.recordList = JSON.parse(window.localStorage.getItem('recordList') || '[]') as RecordItem[];
@@ -50,18 +51,46 @@ const store = new Vuex.Store({
       const names = state.tagList.map(item => item.name);
       if (names.indexOf(name) >= 0) {
         window.alert('标签重复');
-        return 'duplicated';
       }
       state.tagList.push({id, name: name});
       store.commit('saveTags');
       window.alert('创建标签成功');
-      return 'success';
     },
     saveTags(state) {
       window.localStorage.setItem('tagList', JSON.stringify(state.tagList));
     },
-    setCurrentTag(state,id: string){
+    setCurrentTag(state, id: string) {
       state.currentTag = state.tagList.filter(t => t.id === id)[0];
+    },
+    updateTag(state, payload: { id: string; name: string }) {
+      const idList = state.tagList.map(item => item.id);
+      const {id, name} = payload;
+      if (idList.indexOf(id) >= 0) {
+        const names = state.tagList.map(item => item.name);
+        if (names.indexOf(name) >= 0) {
+          window.alert('标签名重复了');
+        } else {
+          const tag = state.tagList.filter(item => item.id === id)[0];
+          tag.name = name;
+          store.commit('saveTags');
+        }
+      }
+    },
+    removeTag(state, id: string) {
+      let index = -1;
+      for (let i = 0; i < state.tagList.length; i++) {
+        if (state.tagList[i].id === id) {
+          index = i;
+          break;
+        }
+      }
+      if (index >= 0) {
+        state.tagList.splice(index, 1);
+        store.commit('saveTags');
+        router.back();
+      } else {
+        window.alert('删除失败');
+      }
     }
   },
 });
