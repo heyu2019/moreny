25fd85b04f23fcea639348ff1e8a0bdc74983fe5
heyu2019 <615791941@qq.com>
2020-05-11 17:59:05 +0800

    删除types.vue

--- src/views/Money.vue
+++ src/views/Money.vue
@@ -14,7 +14,6 @@
 <script lang="ts">
   import Vue from 'vue';
   import NumberPad from '@/components/Money/NumberPad.vue';
-  import Types from '@/components/Money/Types.vue';
   import FormItem from '@/components/Money/FormItem.vue';
   import Tags from '@/components/Money/Tags.vue';
   import {Component} from 'vue-property-decorator';
@@ -35,7 +34,7 @@
     window.localStorage.setItem('version', '0.0.2');*/
 
   @Component({
-    components: {Tabs, Tags, FormItem, Types, NumberPad},
+    components: {Tabs, Tags, FormItem, NumberPad},
   })
   export default class Money extends Vue {
     record: RecordItem = {tags: [], notes: '', type: '-', amount: 0, createdAt: ''};
