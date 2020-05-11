b660dcdafb730995610ac57f366a935367c7a319
heyu2019 <615791941@qq.com>
2020-05-11 17:10:02 +0800

    抽离tab.vaue,使用在statistic.vue里

--- src/components/Money/Types.vue
+++ src/components/Money/Types.vue
@@ -1,9 +1,9 @@
 <template>
   <ul class="types">
-    <li :class="value==='-' && 'selected'"
+    <li :class="{[classPrefix+'-item']:classPrefix,selected:value==='-'}"
         @click="selectedType('-')">支出
     </li>
-    <li :class="value==='+' && 'selected'"
+    <li :class="{[classPrefix+'-item']:classPrefix,selected:value==='+'}"
         @click="selectedType('+')">收入
     </li>
   </ul>
@@ -11,13 +11,14 @@
 
 <script lang="ts">
   import Vue from 'vue';
-  import {Component, Prop, Watch} from 'vue-property-decorator';
+  import {Component, Prop} from 'vue-property-decorator';
   //文档提示更智能
   //不能随便写toString
   //编译报错
   @Component
   export default class Types extends Vue {
-    @Prop() readonly value!: string;
+    @Prop(String) readonly value!: string;
+    @Prop(String) classPrefix?: string;
 
     //type = '-'; //'-'表示支出 '+'表示收入
     selectedType(type: string) {//type只能是'-'或者'+'
---
+++ src/components/Tabs.vue
@@ -0,0 +1,65 @@
+<template>
+  <ul class="tabs">
+    <li v-for="item in dataSource"
+        :class="liClass(item)"
+        :key="item.value"
+        @click="selected(item)">
+      {{item.text}}
+    </li>
+  </ul>
+</template>
+
+<script lang="ts">
+  import Vue from 'vue';
+  import {Component, Prop} from 'vue-property-decorator';
+
+  type DataSourceItem = {text: string;value: string};
+  @Component
+  export default class Tabs extends Vue {
+    @Prop({required: true, type: Array})
+    dataSource!: DataSourceItem[];
+    @Prop(String) readonly value!: string;
+    @Prop(String) classPrefix?: string;
+
+    liClass(item: DataSourceItem){
+      return {
+        selected:item.value===this.value
+        ,[this.classPrefix+'-tabs-item']:this.classPrefix
+      }
+    }
+    selected(item: DataSourceItem){
+      this.$emit('update:value',item.value)
+    }
+  }
+</script>
+
+<style lang="scss" scoped>
+  .tabs {
+    background: #c4c4c4;
+    display: flex;
+    text-align: center;
+    font-size: 24px;
+
+    > li {
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      width: 50%;
+      height: 64px;
+
+      &.selected {
+        position: relative;
+
+        &::after {
+          content: '';
+          position: absolute;
+          bottom: 0;
+          left: 0;
+          width: 100%;
+          height: 4px;
+          background: #333333;
+        }
+      }
+    }
+  }
+</style>
