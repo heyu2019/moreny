30a45a09f41a99863da31c6a0a0d964408dffedc
heyu2019 <615791941@qq.com>
2020-05-14 21:31:11 +0800

    使用户获得更好的浏览体验

--- src/main.ts
+++ src/main.ts
@@ -25,3 +25,15 @@ window.onload = function () {
     window.scrollTo(0, 10000);
   },0);//解决手机上显示会出现下巴的问题
 };
+
+if (document.documentElement.clientHeight>500){
+  window.alert('请使用手机打开本页面，以此保证浏览效果');
+  const img = document.createElement('img')
+  img.src='./saoma.png';
+  img.style.position = 'fixed';
+  img.style.left='50%';
+  img.style.top='50%';
+  img.style.boxShadow='0 0 10px rgba(0,0,0.25)';
+  img.style.transform='translate(-50%,-50%)';
+  document.body.appendChild(img)
+}
