1329c086ad92612c3725fe3fc1cd4f93538bc58a
heyu2019 <615791941@qq.com>
2020-05-14 21:19:00 +0800

    部署网站moerny到YuPocketbook

--- vue.config.js
+++ vue.config.js
@@ -3,18 +3,22 @@ const path = require('path');
 module.exports = {
   lintOnSave: false,
 
-  chainWebpack: config=>{
-    const dir = path.resolve(__dirname,'src/assets/icons')
-
+  chainWebpack: config => {
+    const dir = path.resolve(__dirname, 'src/assets/icons')
+    process.env.NODE_ENV === 'production'
+      ? '/YuPocketbook/'
+      : '/',
     config.module
       .rule('svg-sprite')
       .test(/\.svg$/)
       .include.add(dir).end() // 只包含icons目录
-      .use('svg-sprite-loader').loader('svg-sprite-loader').options({extract:false}).end()
+      .use('svg-sprite-loader').loader('svg-sprite-loader').options({extract: false}).end()
       .use("svgo-loader").loader("svgo-loader")
-      .tap(options=>({...options,plugins: [{removeAttrs: {attrs: "path:fill"}}]})).end()
+      .tap(options => ({...options, plugins: [{removeAttrs: {attrs: "path:fill"}}]})).end()
 
     config.plugin('svg-sprite').use(require('svg-sprite-loader/plugin'), [{plainSprite: true}])
     config.module.rule('svg').exclude.add(dir) //其他svg loader排除icons目录
+
+
   }
 }
